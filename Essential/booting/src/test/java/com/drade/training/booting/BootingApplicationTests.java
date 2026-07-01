5f0d2ed470ef68a19dc0db9c1b2d5a42e6a870fd
Daninator001 <daninator001@gmail.com>
2026-07-01 11:05:30 +0200

    01_01

---
+++ Essential/booting/src/test/java/com/drade/training/booting/BootingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drade.training.booting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
